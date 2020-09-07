d88db719f14048028f1688bd8a47b99055b38180
CRS <CRS@192.168.0.198>
2020-09-07 17:08:02 +0200

    First Change

--- Hello Java/src/hellojava.java
+++ Hello Java/src/hellojava.java
@@ -4,6 +4,7 @@ public class hellojava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello java");
+	System.out.println("changes");
 	}
 
 }
