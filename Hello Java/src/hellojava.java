1b5bdc3753ed7285091052ae25fb03e639a4bddd
CRS <CRS@Stefan-PC>
2020-10-02 17:22:05 +0200

    Added Json library

--- Hello Java/src/hellojava.java
+++ Hello Java/src/hellojava.java
@@ -1,10 +1,16 @@
-
+import org.json.*;
 public class hellojava {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JSONException {
 		// TODO Auto-generated method stub
 		System.out.println("hello java");
 	System.out.println("changes");
+	JSONObject f = new JSONObject();
+	f.put("Emer", "fani");
+	f.put("Mbiemer", "gjoni");
+	f.put("mosha", 30);
+	
+	System.out.println(f);
 	}
 
 }
