247e6625a1db87eca3e4affff25aa49a8cfac9c7
CRS <CRS@192.168.0.194>
2020-10-10 13:44:28 +0200

    new update

--- Hello Java/src/hellojava.java
+++ Hello Java/src/hellojava.java
@@ -5,6 +5,7 @@ public class hellojava {
 		// TODO Auto-generated method stub
 		System.out.println("hello java");
 	System.out.println("changes");
+	System.out.println("Hello Bosi");
 	JSONObject f = new JSONObject();
 	f.put("Emer", "fani");
 	f.put("Mbiemer", "gjoni");
